8866c25a45eb3048143942da3cd72bf5c7f68957
SarahZhao806 <yzhao806@gmail.com>
2016-08-28 20:27:00 -0400

    Added nav bar

--- includes/timeline.js
+++ includes/timeline.js
@@ -2,3 +2,24 @@
 
 
 
+var timelineWidth = 0;
+var panelWidth = 0;
+var firstRun = true;
+var totalPanel = 0;
+var currentdPanel = 0;
+
+$(document).ready(function() {
+   panelWidth = $('.timeline .panel').width(); 
+    
+   adjustLayout();
+});
+
+function adjustLayout() {
+    $('.timeline .panel').each(function(index) {
+        var newX = panelWidth * index;
+        $(this).css('left', newX + 'px');
+        
+        var newLabel = $(this).find('.label').html();
+        $('.timeline nav').append('<a href="#">' + newLabel + '</a>');
+    });
+}
